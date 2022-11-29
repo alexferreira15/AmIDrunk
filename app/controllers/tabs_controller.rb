e1554dcad3fd5ceb842f79be8bb471c7c217d8dc
Andre Sardinha <acsardinha@hotmail.com>
2022-11-29 17:17:12 +0000

    Tabs Controller - Not finished

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -1,3 +1,18 @@
 class TabsController < ApplicationController
+  def index
+    @tabs = Tab.all
+  end
+
+  def create
+    @tab = Tab.new(fd_params)
+    @tab.save!
+    # redirect_to
+  end
+
+  def update
+    @tab = Tab.find(params[:id])
+    @tab.save!
+    # redirect_to
+  end
 
 end
