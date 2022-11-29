2a4b4cdc917a54c40fe790959c751c43fef3ab63
Andre Sardinha <acsardinha@hotmail.com>
2022-11-29 15:49:32 +0000

    Tabs - routes and controller *controller* not finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :users, only: [] do
+    resources :tabs, only: %i[index create update]
+  end
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
