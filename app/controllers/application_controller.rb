fed2982f86468c7ccc8c7e530dac16fd1d87bccb
Alexandre Ferreira <alex.ferreira9915@gmail.com>
2022-12-01 12:21:46 +0000

    cloudinary working for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit::Authorization
-  
+
   # Pundit: allow-list approach
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
-  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :gender, :weight, :height, :username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :gender, :weight, :height, :username, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :gender, :weight, :height, :username])
